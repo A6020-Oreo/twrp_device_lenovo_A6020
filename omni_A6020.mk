4745e18dc96fd3b0206356e9f775502c9b7a341e
Yash Garg <ben10.yashgarg@gmail.com>
2018-04-07 13:30:40 +0530

    Update omni_A6020.mk

--- omni_A6020.mk
+++ omni_A6020.mk
@@ -25,10 +25,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit language packages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Kernel
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/Image.gz-dtb:kernel
   
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := A6020
